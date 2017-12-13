625e3fcb36d831d39fc485cd07d1ec08e3c9f017
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-12-13 17:12:49 -0600

    Custom directive that will calculate the URL of an asset

---
+++ orcid-web/src/main/java/org/orcid/frontend/web/freemarker/AssetDirective.java
@@ -0,0 +1,42 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.frontend.web.freemarker;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+import freemarker.core.Environment;
+import freemarker.template.TemplateDirectiveBody;
+import freemarker.template.TemplateDirectiveModel;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateModel;
+
+public class AssetDirective implements TemplateDirectiveModel {
+
+    //https://freemarker.apache.org/docs/pgui_datamodel_directive.html
+    
+    @Override
+    public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body) throws TemplateException, IOException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public static class AssetUrlGeneratorWriter extends Writer {
+        
+    }
+}
